f6698a87443b10c20713399d86a78a20b7e778a3
Jesse P. Johnson <kuwv@users.noreply.github.com>
2021-01-11 12:13:50 -0500

    Update dependencies.py
    code example not mine

--- pkgmgr/dependencies.py
+++ pkgmgr/dependencies.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# copyright: (c) 2020 by Jesse Johnson.
-# license: Apache 2.0, see LICENSE for more details.
 '''Provide dependency management.'''
 
 from semver import Version
