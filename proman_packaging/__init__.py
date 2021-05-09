b63d182980435bda17a5e00edec5d28c65317035
Jesse P. Johnson <jpj6652@gmail.com>
2021-05-09 10:45:07 -0400

    Bump version: 0.1.1-dev2 → 0.1.1-dev3

--- proman_packaging/__init__.py
+++ proman_packaging/__init__.py
@@ -6,7 +6,7 @@ import logging
 
 __author__ = 'Jesse P. Johnson'
 __title__ = 'proman-packaging'
-__version__ = '0.1.1-dev2'
+__version__ = '0.1.1-dev3'
 __license__ = 'Apache-2.0'
 __all__: List[str] = []
 
